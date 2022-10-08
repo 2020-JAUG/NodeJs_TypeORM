5b0dc8728975cefb4d4d3ea481a44701f1530056
Alejandro Urbina <urbinagonzalezja@gmail.com>
2022-10-08 21:19:33 +0200

    Create server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,51 @@
+import 'reflect-metadata';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import {DataSource} from "typeorm";
+import {ConfigServer} from "./config/config";
+import {UserRouter} from "./user/user.router";
 
+export class Server extends ConfigServer {
 
-export class Server {
+    public app: express.Application = express();
+    //Con la palabra reservado this accedemos al metodo getNumberEnv gracias a la herencia y la palabra reservada super
+    private port: number = this.getNumberEnv("PORT");// Nombre de la varibale de entorno que tenemos en el .env
 
+    constructor() {
+
+        super();
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+
+        let ignore = this.dbConnection();
+        this.app.use(morgan('dev'));
+        this.app.use(cors());
+
+        this.app.use('/api', this.routers());
+        this.listen();
+    }
+
+    routers(): express.Router[] {
+        return [
+            new UserRouter().router
+        ]
+    }
+
+    async dbConnection(): Promise< DataSource | void> {
+        return this.initConnect
+            .then(() => {
+                console.log("Connect Success!");
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+    public listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Server listening on port ${this.port}`);
+        })
+    }
 }
+
+new Server();

141258ffc97d9eaf5daf7caaa5907f1e15cbd5ac
Alejandro Urbina <urbinagonzalezja@gmail.com>
2022-10-08 21:21:40 +0200

    Create dbConnect

---
+++ src/config/data.source.ts
@@ -0,0 +1,28 @@
+import {DataSource, DataSourceOptions} from "typeorm";
+import * as dotenv from 'dotenv';
+import {SnakeNamingStrategy} from "typeorm-naming-strategies";
+import {database} from "../mysql/constants";
+
+//Lee las variables de entorno
+dotenv.config({
+    path: process.env.NODE_ENV !== undefined ? `.${process.env.NODE_ENV.trim()}.env`
+        : ".env"
+})
+
+const Config: DataSourceOptions = {
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.MYSQL_USER || database.user,
+    password: process.env.MYSQL_PASSWORD || database.password,
+    database: process.env.MYSQL_DATABASE || database.database,
+    entities: [__dirname + "/../**/*.entity{.ts,.js}"], //Para leer las entidades del proyecto y buscar en las carpetas los archivos que tengan la extension .ts .js
+    migrations: [__dirname + "/../migrations/*{.ts,.js}"],
+    synchronize: true,
+    migrationsRun: false,
+    logging: false,
+    namingStrategy: new SnakeNamingStrategy(), //Para almacenar en la base de datos los nombres en snake_case
+};
+
+
+export const AppDataSource: DataSource = new DataSource(Config);

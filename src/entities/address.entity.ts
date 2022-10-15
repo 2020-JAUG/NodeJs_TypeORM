c10150d0b61b47e3a4c3f09b98b04d4eadc14841
Alejandro Urbina <urbinagonzalezja@gmail.com>
2022-10-15 17:34:14 +0200

    Create address entoty

---
+++ src/entities/address.entity.ts
@@ -0,0 +1,38 @@
+import {BaseEntity} from "../config/base.entity";
+import {Column, Entity, JoinColumn, ManyToOne} from "typeorm";
+import {CustomerEntity} from "./customer.entity";
+
+@Entity({ name: "addresses"})
+export class AddressEntity extends BaseEntity {
+
+    @ManyToOne(() => CustomerEntity, customer => customer.addresses)
+    @JoinColumn({ name: 'customer_id'})
+    customer: CustomerEntity;
+
+    @Column()
+    street_name: string;
+
+    @Column()
+    number: string;
+
+    @Column()
+    staircase: string;
+
+    @Column()
+    floor: string;
+
+    @Column()
+    door: string;
+
+    @Column()
+    postal_code: number;
+
+    @Column()
+    locality: string;
+
+    @Column()
+    province: string;
+
+    @Column()
+    country: string;
+}

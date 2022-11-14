51314274ed34278c4171dc1bf53b458ddf75b306
Alejandro Urbina <urbinagonzalezja@gmail.com>
2022-11-15 00:13:05 +0100

    Add method to create a customer

--- src/services/customer.service.ts
+++ src/services/customer.service.ts
@@ -1,19 +1,37 @@
-import {DeleteResult, UpdateResult} from "typeorm";
-import {CustomerEntity} from "../entities/customer.entity";
-import {BaseService} from "../config/base.service";
-import {CustomerDto} from "../entities/dto/customer.dto";
-import {AppDataSource} from "../config/data.source";
+import { DeleteResult, UpdateResult } from "typeorm";
+import { CustomerEntity } from "../entities/customer.entity";
+import { BaseService } from "../config/base.service";
+import { CustomerDto } from "../entities/dto/customer.dto";
+import { AppDataSource } from "../config/data.source";
+import { UserService } from "./user.service";
+import { RoleType } from "../entities/dto/user.dto";
 
 export class CustomerService extends BaseService<CustomerEntity> {
 
     protected repository = AppDataSource.getRepository(CustomerEntity);
 
-    constructor() {
+    constructor(private readonly userService: UserService = new UserService()) {
         super(CustomerEntity)
     }
 
-    async createCustomer(body: CustomerDto): Promise<CustomerEntity> {
-        return ( await this.execRepository).save(body);
+    async createCustomer(body: CustomerDto): Promise<CustomerEntity | null> {
+
+        const createCustomer = (await this.execRepository).create(body);
+        //const createCustomer = new CustomerEntity()
+        //createCustomer.user = body.user;
+        //createCustomer.identity_card = body.identity_card;
+
+        const user = await this.userService.findUserById(createCustomer.user.id);
+
+        if (user) {
+            await this.userService.updateUser(user.id, {
+                ...user,
+                role: RoleType.CUSTOMER,
+            });
+
+            return (await this.execRepository).save(body);
+        }
+        return null;
     }
 
     async findAllCustomers(): Promise<CustomerEntity[]> {

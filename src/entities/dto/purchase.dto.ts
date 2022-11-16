cc812828222aad9c74c95864b414fce4019e3e3c
Alejandro Urbina <urbinagonzalezja@gmail.com>
2022-11-16 23:33:14 +0100

    Remove fields

--- src/entities/dto/purchase.dto.ts
+++ src/entities/dto/purchase.dto.ts
@@ -1,7 +1,7 @@
-import {IsNotEmpty, IsOptional} from "class-validator";
+import { IsNotEmpty, IsOptional } from "class-validator";
 import { BaseDTO } from "../../config/base.dto";
-import {CustomerEntity} from "../customer.entity";
-import {ProductEntity} from "../product.entity";
+import { CustomerEntity } from "../customer.entity";
+import { ProductEntity } from "../product.entity";
 
 export class PurchaseDTO extends BaseDTO {
     @IsNotEmpty()
@@ -12,15 +12,6 @@ export class PurchaseDTO extends BaseDTO {
 
     @IsNotEmpty()
     customer!: CustomerEntity;
-
-    @IsNotEmpty()
-    quantity_product: number;
-
-    @IsNotEmpty()
-    total_price: number;
-
-    @IsOptional()
-    products!: ProductEntity[];
 }
 
 export enum StatusPurchase {

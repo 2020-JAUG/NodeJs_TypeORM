d68b556651c9581c5b2d9b52fbab23df89449d6c
Alejandro Urbina <urbinagonzalezja@gmail.com>
2022-11-23 23:50:43 +0100

    Create method to check role

---
+++ src/helpers/middlewares/shared.middleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import passport from "passport";
+import { RoleType } from "../../entities/dto/user.dto";
+import { UserEntity } from "../../entities/user.entity";
+import { HttpResponse } from "../http.response";
+
+export class SharedMiddleware {
+
+    constructor(
+        private readonly httpResponse: HttpResponse = new HttpResponse()
+    ) { }
+
+    passAuth(type: string) { //Aqui recibo el nombre de cada strategy
+        return passport.authenticate(type, { session: false });
+    }
+
+    checkAdminRole(req: Request, res: Response, next: NextFunction) {
+
+        const user = req["user"] as UserEntity;
+
+        if (user.role !== RoleType.ADMIN) {
+            return this.httpResponse.Unauthorized(res, "Unauthorizaed!");
+        }
+        return next();
+    }
+}
